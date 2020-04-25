5fe31180f4fbf5cd654c4bf8b6e0fa6bc9387deb
Fluke <jesadakorn.kirtnu@gmail.com>
2020-04-26 03:28:58 +0700

    add bitkub secure endpoint

--- exchanges/bitkub.js
+++ exchanges/bitkub.js
@@ -3,11 +3,24 @@ const API_HOST = 'https://api.bitkub.com',
     BITKUB_KEY = process.env.API_KEY || 'Plase insert API_KEY',
     BITKUB_SECRET = process.env.API_SECRET || 'Please insert API SECRET'
 
-const header = {
-    'accept': 'application/json',
-    'content-type': 'application/json',
-    'x-btk-apikey': BITKUB_KEY,
+let data = {
+    ts: ""
 }
+
+const header = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'X-BTK-APIKEY': BITKUB_KEY,
+}
+
+let signature = () => {
+    return crypto.createHmac('sha256', BITKUB_SECRET)
+        .update(JSON.stringify({ ...data, ts: +data.ts }))
+        .digest('hex')
+}
+
+const sig = signature()
+data['sig'] = sig
 
 const getData = async (url, query) => {
     try {
@@ -18,10 +31,25 @@ const getData = async (url, query) => {
     }
 }
 
+const postData = async () => {
+    try {
+        await axios.post(`${API_HOST}/api/market/wallet`, JSON.stringify({ sig: data.sig, ts: +data.ts }), {
+            headers: header
+        }).then(res => {
+            console.log(res.data)
+        })
+    }
+    catch (e) {
+        console.error(e)
+    }
+}
+
 exports.bitkub = {
     getSymbol: getData(API_HOST, '/api/market/symbols'),
     getServerTime: getData(API_HOST, '/api/servertime'),
     getWalletBalance: () => {
-
+        this.bitkub.getServerTime.then(res => {
+            data['ts'] = res
+        })
     }
 }
