d3a7e9b638293dd045d283a8575fa05ece51a2c9
Fluke <jesadakorn.kirtnu@gmail.com>
2020-05-01 09:14:56 +0700

    fix missed config

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    env: {
+        firebaseConfig : {
+            apiKey: "AIzaSyC2iAmDtMfqUuvyc5poytOLe45RHnyMraI",
+            authDomain: "miniprojectdcw.firebaseapp.com",
+            databaseURL: "https://miniprojectdcw.firebaseio.com",
+            projectId: "miniprojectdcw",
+            storageBucket: "miniprojectdcw.appspot.com",
+            messagingSenderId: "1055900512391",
+            appId: "1:1055900512391:web:31cb3452ee2344fbd58822",
+            measurementId: "G-GST0LHQ694"
+        }
+    },
+}
